fcff3765d460bef252444bc568d236ddb59dc4d6
Pubudu Costa <cobsccomp192p-032@student.nibm.lk>
2022-02-22 11:30:45 +0530

    Add Project

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace GoodBoy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mSGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reStartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(157, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.mSGToolStripMenuItem,
+            this.reStartToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(117, 70);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // mSGToolStripMenuItem
+            // 
+            this.mSGToolStripMenuItem.Name = "mSGToolStripMenuItem";
+            this.mSGToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.mSGToolStripMenuItem.Text = "MSG";
+            this.mSGToolStripMenuItem.Click += new System.EventHandler(this.mSGToolStripMenuItem_Click);
+            // 
+            // reStartToolStripMenuItem
+            // 
+            this.reStartToolStripMenuItem.Name = "reStartToolStripMenuItem";
+            this.reStartToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.reStartToolStripMenuItem.Text = "Re-Start";
+            this.reStartToolStripMenuItem.Click += new System.EventHandler(this.reStartToolStripMenuItem_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(193, 84);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(108, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Auto Run at Start";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::GoodBoy.Properties.Resources.icons8_wildebeest_96;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(137, 210);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(189, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Good Boy";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(253, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "About";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(340, 232);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Good Boy";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mSGToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reStartToolStripMenuItem;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,226 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoodBoy
+{
+    public partial class Form1 : Form
+    {
+
+        public delegate void Notify();  // delegate
+
+
+            [DllImport("wtsapi32.dll")]
+            private static extern bool WTSRegisterSessionNotification(IntPtr hWnd, int dwFlags);
+
+            [DllImport("wtsapi32.dll")]
+            private static extern bool WTSUnRegisterSessionNotification(IntPtr hWnd);
+
+            [FlagsAttribute]
+            public enum EXECUTION_STATE : uint
+            {
+                ES_AWAYMODE_REQUIRED = 0x00000040,
+                ES_CONTINUOUS = 0x80000000,
+                ES_DISPLAY_REQUIRED = 0x00000002,
+                ES_SYSTEM_REQUIRED = 0x00000001
+                // Legacy flag, should not be used.
+                // ES_USER_PRESENT = 0x00000004
+            }
+
+            [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+            private const int SessionChangeMessage = 0x02B1;
+            private const int SessionLockParam = 0x7;
+            private const int SessionUnlockParam = 0x8;
+            private const int NotifyForThisSession = 0; // This session only
+
+            public Form1()
+            {
+                SystemEvents.PowerModeChanged += OnPowerChange;
+
+                WTSRegisterSessionNotification(this.Handle, NotifyForThisSession);
+                PreventSleep();
+                Console.WriteLine("Runnig:");
+                InitializeComponent();
+            }
+
+            void PreventSleep()
+            {
+                // Prevent Idle-to-Sleep (monitor not affected) (see note above)
+                SetThreadExecutionState(EXECUTION_STATE.ES_DISPLAY_REQUIRED | EXECUTION_STATE.ES_CONTINUOUS);
+            }
+
+
+            protected override void WndProc(ref Message m)
+            {
+                // check for session change notifications
+                if (m.Msg == SessionChangeMessage)
+                {
+                    if (m.WParam.ToInt32() == SessionLockParam)
+                        OnSessionLock(); // Do something when locked
+                    else if (m.WParam.ToInt32() == SessionUnlockParam)
+                        OnSessionUnlock(); // Do something when unlocked
+                }
+
+                  if (m.Msg == 0x0112) // WM_SYSCOMMAND
+                  {
+                      if (m.WParam == new IntPtr(0xF020)) // SC_MINIMIZE
+                      {
+                          notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+                          notifyIcon1.BalloonTipText = "Good Boy";//
+                          notifyIcon1.BalloonTipTitle = "Background running..";//
+                          notifyIcon1.ShowBalloonTip(1000);
+
+                          this.WindowState = FormWindowState.Minimized;
+                          this.ShowInTaskbar = false;
+                      }
+                      m.Result = new IntPtr(0);
+                  }
+
+            base.WndProc(ref m);
+                return;
+            }
+
+            private void OnSessionUnlock()
+            {
+            Console.WriteLine("Runnig:OnSessionUnlock");
+
+            //notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            //notifyIcon1.BalloonTipText = "Good Boy";//
+            //notifyIcon1.BalloonTipTitle = "U Are Back";//
+            //notifyIcon1.ShowBalloonTip(1000);
+
+            }
+
+            void OnSessionLock()
+            {
+            Console.WriteLine("Runnig:OnSessionLock");
+
+            //notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            //notifyIcon1.BalloonTipText = "Good Boy";//
+            //notifyIcon1.BalloonTipTitle = "BYE..";//
+            //notifyIcon1.ShowBalloonTip(1000);
+
+            }
+
+            private void OnPowerChange(object s, PowerModeChangedEventArgs e)
+            {
+
+                switch (e.Mode)
+                {
+                    case PowerModes.Resume:
+                        Console.WriteLine("Runnig:Resume");
+                        break;
+                    case PowerModes.Suspend:
+                        Console.WriteLine("Runnig:Suspend");
+
+                    notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+                    notifyIcon1.BalloonTipText = "Good Boy";//
+                    notifyIcon1.BalloonTipTitle = "C U Tomorrow";//
+                    notifyIcon1.ShowBalloonTip(1000);
+
+                    break;
+                    case PowerModes.StatusChange:
+                        Console.WriteLine("Runnig:StatusChanged");
+                        break;
+                }
+
+           
+
+            }
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+                Cursor = new Cursor(Cursor.Current.Handle);
+                Cursor.Position = new Point(Cursor.Position.X - 20, Cursor.Position.Y - 20);
+                Cursor.Position = new Point(Cursor.Position.X - 20, Cursor.Position.Y - 20);
+                Cursor.Position = new Point(Cursor.Position.X - 20, Cursor.Position.Y - 20);
+
+
+
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.BalloonTipText = "Minimized";//
+            notifyIcon1.BalloonTipTitle = "Background running..";//
+            notifyIcon1.ShowBalloonTip(1000);
+
+
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+                this.WindowState = FormWindowState.Normal;
+
+            this.Activate();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+                this.WindowState = FormWindowState.Normal;
+
+            this.Activate();
+        }
+
+        private void mSGToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.BalloonTipText = "Minimized";//
+            notifyIcon1.BalloonTipTitle = "Background running..";//
+            notifyIcon1.ShowBalloonTip(1000);
+
+        }
+
+        private void reStartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+            Environment.Exit(0);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            RegistryKey reg = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            if (checkBox1.Checked)
+            {
+                reg.SetValue("Good Boy", Application.ExecutablePath.ToString());
+                MessageBox.Show("Message", "ok", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                reg.DeleteValue("Good Boy", true);
+                MessageBox.Show("Deleted", "Deleted", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Develop By PUBUDU", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+
+            MessageBox.Show("Good Boy is now turn off.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+        }
+    }
+}
